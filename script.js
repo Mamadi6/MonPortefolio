8db7cb4f38a5019cca0aed5711fe1b51fb0183ae
mamadi samassa <samassamamadi1@gmail.com>
2025-11-15 16:28:54 +0100

    suppression de l'animation jquery

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 $(function(){
-    $("*").hide();
-    $("*").show(5000);
+    // $("*").hide();
+    // $("*").show(5000);
 
-    $("#body").hide();
+    // $("#body").hide();
     
-    $("#contexte").hide();
-    $("#contexte").show(5000).css('font-size', '130px').slideDown();
-    $("#contexte").css('position', 'relative');
-    $("#contexte").css('background-image','url("/image/photoportefolio.png")');
-    $("#contexte").css('background-size','cover');
-    $("#contexte").css('background-repeat','no-repeat');
-    $("#contexte").css('background-color', 'cyan');
-    $("#contexte").css('color', 'white');
-    $("#contexte").css('text-align', 'center');
+    // $("#contexte").hide();
+    // $("#contexte").show(5000).css('font-size', '130px').slideDown();
+    // $("#contexte").css('position', 'relative');
+    // $("#contexte").css('background-image','url("/image/photoportefolio.png")');
+    // $("#contexte").css('background-size','cover');
+    // $("#contexte").css('background-repeat','no-repeat');
+    // $("#contexte").css('background-color', 'cyan');
+    // $("#contexte").css('color', 'white');
+    // $("#contexte").css('text-align', 'center');
 
-    $("#non").click(function(){
-        $("#oui").empty();
-        $("#non").empty();
-        $("#question").empty();
-        $(this).text("AU REVOIR").css('font-size', '150px');
+    // $("#non").click(function(){
+    //     $("#oui").empty();
+    //     $("#non").empty();
+    //     $("#question").empty();
+    //     $(this).text("AU REVOIR").css('font-size', '150px');
 
-    });
+    // });
 
-    $("#oui").click(function(){
-        $("#non").hide();
-        $("#body").show();
-        $("#contexte").remove();
+    // $("#oui").click(function(){
+    //     $("#non").hide();
+    //     $("#body").show();
+    //     $("#contexte").remove();
 
-    });
+    // });
 
-    $("#oui").on('click', function(e){
-        e.preventDefault();
-        $("#oui").empty();
-        $("#non").empty();
-        $("#question").empty();
-        $("#body").show();
+    // $("#oui").on('click', function(e){
+    //     e.preventDefault();
+    //     $("#oui").empty();
+    //     $("#non").empty();
+    //     $("#question").empty();
+    //     $("#body").show();
 
-    })
+    // })
 
     $("form").on('submit', function(){
         const nom = $("#nom").val().trim();
